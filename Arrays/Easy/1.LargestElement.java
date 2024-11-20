66af7a66c2e9cab9f5f5d73bad6dc7f1a1ffb22d
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-11-20 10:29:48 +0530

    Arrays Easy Q1

--- Arrays/Easy/1.LargestElement.java
+++ Arrays/Easy/1.LargestElement.java
@@ -1,6 +1,11 @@
 /*
     Largest Element in Array
     Given an array arr[]. The task is to find the largest element and return it.
+
+    Link: https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0
+
+    Time Complexity: O(n)
+    Auxilary Space: O(1)
 */
 
 class Solution {
@@ -12,3 +17,16 @@ class Solution {
         return max;
     }
 }
+
+/*
+    Alternative Approach(Using Sorting):
+    Time Complexity: O(nlog(n))
+    Space Complexity: O(1)
+
+    class Solution {
+        public static int largest(int[] arr) {
+            Arrays.sort(arr);
+            return arr[arr.length - 1];
+        }
+    }
+*/
